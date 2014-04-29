33a9b73f286c3079b09043557c991cb589ea2c73
Paul Craig <pcraig3@uwo.ca>
2014-04-29 17:55:57 -0400

    Initial commit of new plugin.

---
+++ README.txt
@@ -0,0 +1,46 @@
+=== Testwestern_Testplugin ===
+Contributors: pcraig3
+Tags: demo, testing
+Requires at least: 3.8
+Tested up to: 3.9
+Stable tag: 0.9.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Aside from figuring out plugins, this plugin is meant to beam in some info from GitHub.
+
+== Description ==
+
+Aside from figuring out plugins, this plugin is meant to beam in some info from GitHub.
+
+Long & short is I'm going to try and get some information out of GitHub to correspond to WordPress shortcodes.
+
+
+== Frequently Asked Questions ==
+
+= Why doesn't it work yet? =
+
+Shut up.
+
+
+== Screenshots ==
+
+1. This screen shot description corresponds to screenshot-1.(png|jpg|jpeg|gif). Note that the screenshot is taken from
+the /assets directory or the directory that contains the stable readme.txt (tags or trunk). Screenshots in the /assets
+directory take precedence. For example, `/assets/screenshot-1.png` would win over `/tags/4.3/screenshot-1.png`
+(or jpg, jpeg, gif).
+2. This is the second screen shot
+
+== Changelog ==
+
+= 0.9 =
+* Totally new.  Renaming everything.
+
+== Upgrade Notice ==
+
+= 0.9 =
+Please don't use this version for anything.
+
+== Where it's due ==
+
+The basic structure of this plugin was cloned from the [WordPress-Plugin-Boilerplate](https://github.com/tommcfarlin/WordPress-Plugin-Boilerplate) project.
---
+++ testwestern-testplugin.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * A Test Plugin that will eventually connect to an API.
+ *
+ * A foundation off of which to build well-documented WordPress plugins that
+ * also follow WordPress Coding Standards and PHP best practices.
+ *
+ * @package   Testwestern_Testplugin
+ * @author    Paul Craig <pcraig3@uwo.ca>
+ * @license   GPL-2.0+
+ * @link      http://testwestern.com
+ * @copyright 2014 University Students' Council
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Testwestern_Testplugin
+ * Plugin URI:        http://testwestern.com
+ * Description:       Does nothing right now.
+ * Version:           0.9.1
+ * Author:            Paul Craig
+ * Author URI:        https://profiles.wordpress.org/pcraig3/
+ * Text Domain:       testwestern-testplugin
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Domain Path:       /languages
+ * GitHub Plugin URI: https://github.com/pcraig3/testwestern-testplugin
+ * GitHub Branch:     develop
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+/*----------------------------------------------------------------------------*
+ * Public-Facing Functionality
+ *----------------------------------------------------------------------------*/
+
+require_once( plugin_dir_path( __FILE__ ) . 'public/class-testwestern-testplugin.php' );
+
+/*
+ * Register hooks that are fired when the plugin is activated or deactivated.
+ * When the plugin is deleted, the uninstall.php file is loaded.
+ *
+ * - rename Plugin_Name in `class-testwestern-testplugin.php`
+ */
+register_activation_hook( __FILE__, array( 'Testwestern_Testplugin', 'activate' ) );
+register_deactivation_hook( __FILE__, array( 'Testwestern_Testplugin', 'deactivate' ) );
+
+/*
+ * - rename Plugin_Name in `class-testwestern-testplugin.php`
+ */
+add_action( 'plugins_loaded', array( 'Testwestern_Testplugin', 'get_instance' ) );
+
+/*----------------------------------------------------------------------------*
+ * Dashboard and Administrative Functionality
+ *----------------------------------------------------------------------------*/
+
+/*
+ * @TODO:
+ *
+ * - replace Plugin_Name_Admin with the name of the class defined in
+ *   `testwestern-testplugin-admin.php`
+ *
+ * If you want to include Ajax within the dashboard, change the following
+ * conditional to:
+ *
+ * if ( is_admin() ) {
+ *   ...
+ * }
+ *
+ * The code below is intended to to give the lightest footprint possible.
+ *
+if ( is_admin() && ( ! defined( 'DOING_AJAX' ) || ! DOING_AJAX ) ) {
+
+	require_once( plugin_dir_path( __FILE__ ) . 'admin/testwestern-testplugin-admin.php' );
+	add_action( 'plugins_loaded', array( 'Plugin_Name_Admin', 'get_instance' ) );
+
+}
+ */
